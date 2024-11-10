0d6674e8ad1e5b88f95c6a41abf85effc3f50148
KauanMO <kauaanmatheus@gmail.com>
2024-11-10 12:11:26 -0300

    Adicionar arquivos de projeto.

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Duck_Mail.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Duck_Mail.Data;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions opts) : base(opts) { }
+
+    public DbSet<EmailTemplate> EmailTemplates { get; set; }
+}
