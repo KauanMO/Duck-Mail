6287defbac016d20ba5c57d482eeb37e9d1e8b18
KauanMO <kauaanmatheus@gmail.com>
2024-11-10 12:38:01 -0300

    fix: CampaignEmailTemplate CreatedDate default date

--- Models/CampaignEmailTemplate.cs
+++ Models/CampaignEmailTemplate.cs
@@ -7,7 +7,7 @@ public class CampaignEmailTemplate
     [Key]
     public int Id { get; set; }
     public string Url { get; set; }
-    public DateTime CreatedDate { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
     public virtual Campaign Campaign { get; set; }
     public virtual EmailTemplate EmailTemplate { get; set; }
 }
